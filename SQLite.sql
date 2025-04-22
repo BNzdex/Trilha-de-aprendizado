368261e21cf55032be9ccd686be79c955bb9fcf6
BNzdex <bernardolocatelli28@gmail.com>
2025-04-22 09:56:28 -0300

    Update SQLite.sql

--- SQLite.sql
+++ SQLite.sql
@@ -1,3 +1,18 @@
+CREATE TABLE Cliente (
+Id_cliente INTEGER PRIMARY KEY AUTOINCREMENT,
+nome_cliente VARCHAR(100) not NULL,
+cpf CHAR(11) NOT NULL,
+endereço VARCHAR(100) NOT NULL
+);
+  
+  
+CREATE TABLE Produtos (
+Id_produto INTEGER PRIMARY KEY AUTOINCREMENT,
+nome_produto VARCHAR(100) NOT NULL,
+quantidade_estoque INT NOT NULL,
+valor VARCHAR(11)
+ );
+
 INSERT INTO Cliente (nome_cliente,cpf,endereço)  VALUES
 ('Bernardo Locatelli', '17397323758', 'Rua Uruguai, 179'),
 ('Lorrany', '98976598123', 'Rua Alfonso Claudio, 99'),
