a02a4cb311344b33dcc4b5f580a53902100660db
BNzdex <bernardolocatelli28@gmail.com>
2025-04-22 09:51:01 -0300

    Add files via upload

--- SQLite.sql
+++ SQLite.sql
@@ -1,13 +1,13 @@
-CREATE TABLE Cliente (
-Id_cliente INT PRIMARY KEY,
-nome_cliente VARCHAR(100) not NULL,
-cpf CHAR(11) NOT NULL,
-endereço VARCHAR(100) NOT NULL
-);
-  
-  
-CREATE TABLE Produtos (
-Id_produto INT PRIMARY KEY,
-nome_produto VARCHAR(100) NOT NULL,
-quantidade_estoque INT NOT NULL
- );
+INSERT INTO Cliente (nome_cliente,cpf,endereço)  VALUES
+('Bernardo Locatelli', '17397323758', 'Rua Uruguai, 179'),
+('Lorrany', '98976598123', 'Rua Alfonso Claudio, 99'),
+('Maria', '62351749629', 'Rua XI, 157'), 
+('Ivo', '84325128552', 'Rua Edilson Santos, 876'),
+('Lolo', '45183139784', 'Rua X, 77');
+
+INSERT INTO Produtos(nome_produto, quantidade_estoque, valor) VALUES
+('Mouse', 55, 'R$30,00'),
+('Teclado', 64, 'R$50,00'),
+('Placa de vídeo', 13, 'R$2000,00'),
+('Fone', 20, 'R$200,00'),
+('Cadeira gamer', 5, 'R$800,00');
