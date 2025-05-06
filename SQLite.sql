8595b551427b910c6b5b80ff609c14fe53c2a472
BNzdex <bernardolocatelli28@gmail.com>
2025-05-06 07:50:02 -0300

    Update SQLite.sql

--- SQLite.sql
+++ SQLite.sql
@@ -14,7 +14,7 @@ quantidade_estoque INT NOT NULL,
 valor INT
  );
 
-INSERT INTO Cliente (nome_cliente,cpf,endereço)  VALUES
+INSERT INTO Cliente (nome_cliente,cpf,endereço, telefone)  VALUES
 ('Bernardo Locatelli', '17397323758', 'Rua Uruguai, 179', '27996283448'),
 ('Lorrany', '98976598123', 'Rua Alfonso Claudio, 99', '27996783888'),
 ('Maria', '62351749629', 'Rua XI, 157', '27991287148'), 
@@ -22,8 +22,8 @@ INSERT INTO Cliente (nome_cliente,cpf,endereço)  VALUES
 ('Lolo', '45183139784', 'Rua X, 77', '27994723448');
 
 INSERT INTO Produtos(nome_produto, quantidade_estoque, valor) VALUES
-('Mouse', 55, '30'),
-('Teclado', 64, '50'),
-('Placa de vídeo', 13, '2000'),
-('Fone', 20, '200'),
-('Cadeira gamer', 5, '800');
+('Mouse', 55, 30),
+('Teclado', 64, 50),
+('Placa de vídeo', 13, 2000),
+('Fone', 20, 200),
+('Cadeira gamer', 5, 800);
