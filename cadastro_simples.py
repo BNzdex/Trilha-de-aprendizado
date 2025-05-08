938194e5bd1bca55df68b1f66d87e5569c4f22cf
BNzdex <bernardolocatelli28@gmail.com>
2025-05-08 08:02:18 -0300

    Add files via upload

---
+++ cadastro_simples.py
@@ -0,0 +1,17 @@
+nome = input('\nDigite seu nome :')
+email = input('Digite sei e-mail :')
+telefone = int(input('Digite seu telefone :'))
+
+print('\nAgora informe as informações do produto\n')
+
+nome_produto = input('Digite o nome do produto :')
+preco = float(input('Informe o preço do produto : '))
+quantidade = int(input('Informe a quantidade de produtos no estoque : '))
+
+print(f'\nO nome do cliente é {nome}, seu e-mail é {email} e seu telefone é {telefone}')
+print(f'O produto é {nome_produto}, o preço é {preco} e a quantidade de produtos no estoque é {quantidade}\n')
+
+
+
+
+
